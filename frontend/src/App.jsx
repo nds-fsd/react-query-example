20cc32f1b1236157fc99cf626e73400a810b2929
Jose Manuel Cano <jmcano94@gmail.com>
2023-04-20 21:09:03 +0200

    Axios with useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,69 +1,76 @@
+import { useEffect, useState } from "react"
 import {useForm} from "react-hook-form";
 import {api} from './utils/api';
-import {QueryClientProvider, QueryClient, useQuery, useQueryClient, useMutation} from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
+import {QueryClientProvider, QueryClient} from 'react-query';
 
-const TodoItem = ({todo}) => {
-
-  const queryClient = useQueryClient();
+const TodoItem = ({todo, setRefresh}) => {
 
   const deleteTodo = () => {
     api.delete(`/task/${todo._id}`)
     .then(res => console.log(res.data) )
-    .catch(e => console.log(e))
-    .finally(() => queryClient?.invalidateQueries('todos'));
+    .catch()
+    .finally(() => setRefresh(true));
   }
   return (<li onClick={deleteTodo} key={todo._id}>{todo.description}</li>)
 }
 
 const Todos = () => {
-  const queryClient = useQueryClient();
 
-  const {register, handleSubmit, formState: {errors, isSubmitting, isValid}} = useForm();
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [refresh, setRefresh] = useState(true);
+  const {register, handleSubmit, formState: {errors, isSubmitting, isValid}, setValue} = useForm();
 
-  const {data, isLoading} = useQuery('todos', 
-  async () => {
-    const res = await api.get('/task');
-    return res.data;
-  })
+  useEffect(() => {
+
+    if(refresh){
+      setIsLoading(true);
+      api.get(`/task`)
+      .then(res => setTodos([...res.data]))
+      .catch(e => console.log(e))
+      .finally(() => {
+        setIsLoading(false);
+        setRefresh(false)}
+        );
+    }
+    
+
+  }, [refresh]);
+
 
   const addNewTodo = (todoData) => {
     api.post(`/task`, todoData)
-  }
-  const mutation = useMutation(addNewTodo, {
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries('todos')
-    },
-  })
-
-
-  const onSubmit = (todoData) => {
-    mutation.mutate(todoData);
+    .then(res => {
+      setValue('description', undefined);
+    } )
+    .catch(e => console.log(e))
+    .finally(() => setRefresh(true));
   }
 
-  
   return (
     <div style={{display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center'}}>
       <h1>My Todo App</h1>
       {isLoading && (<div>Cargando...</div>)}
       {!isLoading &&  (<ul>
-        {data.map(todo => (
-          <TodoItem todo={todo}/>
+        {todos.map(todo => (
+          <TodoItem todo={todo} setRefresh={setRefresh}/>
         ))}
       </ul>)}
       
       
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(addNewTodo)}>
         <input placeholder="Do the laundry" {...register('description', {required: { value: true, message: 'Describe the task'}})}/>
-        <button type="submit" disabled={!isValid || mutation.isSubmitting}>{mutation.isSubmitting ? 'enviando...' : 'Add Todo'}</button>
+        <button type="submit" disabled={!isValid || isSubmitting}>Add Todo</button>
         {errors.description && <div style={{color: 'red', fontSize: 12}}>{errors.description.message}</div>}
       </form>
      
     </div>
   )
 
+
 }
+
+
 
 const queryClient = new QueryClient()
 
@@ -73,7 +80,6 @@ function App() {
     return (
       <QueryClientProvider client={queryClient}>
         <Todos/>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   )
 }
